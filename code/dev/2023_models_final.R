592a5b1f13904909264ae5ef7f5a1151dc0b6599
Jesse <jesse.kartes@gmail.com>
2023-10-07 17:31:10 -0500

    Update 2023_models_final.R

--- code/dev/2023_models_final.R
+++ code/dev/2023_models_final.R
@@ -8,7 +8,7 @@ library(tidymodels) # model eval
 # library(MLeval) # model eval
 # library(pROC) # model eval
 library(ggfortify) # autoplot
-# library(glmnet) # regularization
+library(glmnet) # regularization
 # library(ranger) # rf
 # library(xgboost) # xgb
 # library(e1071) # svm
@@ -387,6 +387,251 @@ lin_opp_imp <- rownames_to_column(importance[["importance"]], "Var") %>%
     arrange(desc(Overall)) %>%
     head(20)
 lin_opp_imp
+
+
+# team winner ridge regression model ----
+
+# all features
+train <- nba_final %>%
+    filter(season <= 2021) %>%
+    select(team_winner, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+test <- nba_final %>%
+    filter(season > 2021) %>%
+    select(team_winner, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+# highly correlated features removed
+train <- train %>% select(-all_of(cor_cols))
+test <- test %>% select(-all_of(cor_cols))
+
+# normalize features
+pre_proc_val <- preProcess(train[,-1], method = c("center", "scale"))
+
+train[,-1] = predict(pre_proc_val, train[,-1])
+test[,-1] = predict(pre_proc_val, test[,-1])
+
+# model
+ctrl <- trainControl(method = "cv", number = 5, verboseIter = T, 
+                     classProbs = T, summaryFunction = twoClassSummary)
+grid <- expand.grid(
+    alpha = 0, # ridge = 0 / lasso = 1
+    lambda = 10^seq(2, -3, by = -.1)
+)
+reg_win <- train(team_winner ~., data = train,
+                 method = "glmnet",
+                 metric = "ROC",
+                 trControl = ctrl,
+                 tuneGrid = grid)
+
+reg_win
+reg_win$resample
+reg_win$results
+summary(reg_win) # model components
+confusionMatrix(reg_win) # confusion matrix
+glance(reg_win$finalModel) # entire model - tidymodels
+tidy(reg_win$finalModel) # model components - tidymodels
+
+# predictions
+win_pred <- predict(reg_win, test, type = "prob")
+confusionMatrix(test$team_winner,
+                factor(ifelse(win_pred[,1] > 0.5, "win", "loss"), 
+                       levels = c("win","loss")),
+                positive = "win")
+
+away_pred <- as.numeric(win_pred[,1])
+home_pred <- as.numeric(win_pred[,2])
+obs <- test$team_winner
+pred <- factor(ifelse(away_pred > 0.5, "win", "loss"), levels = c("win","loss"))
+obs_pred <- data.frame(obs = obs,
+                       pred = pred,
+                       win = away_pred, 
+                       loss = home_pred)
+
+# model evaluation
+twoClassSummary(obs_pred, lev = levels(obs)) # roc
+prSummary(obs_pred, lev = levels(obs)) # auc
+mnLogLoss(obs_pred, lev = levels(obs)) # log loss
+
+roc_score <- pROC::roc(test$team_winner, away_pred, plot = T, legacy.axes = T,
+                       percent = T,
+                       xlab="False Positive Percentage",
+                       ylab="True Postive Percentage",
+                       col="#377eb8", lwd = 4, print.auc = T) # roc chart
+
+postResample(pred = pred, obs = obs) # caret eval
+
+# feature importance
+importance <- varImp(reg_win, scale = F)
+plot(importance)
+
+reg_win_imp <- rownames_to_column(importance[["importance"]], "Var") %>%
+    arrange(desc(Overall)) %>%
+    head(20)
+reg_win_imp
+
+
+# team score linear regression model ----
+
+# all features
+train <- nba_final %>%
+    filter(season <= 2021) %>%
+    select(team_score, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+test <- nba_final %>%
+    filter(season > 2021) %>%
+    select(team_score, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+# highly correlated features removed
+train <- train %>% select(-all_of(cor_cols))
+test <- test %>% select(-all_of(cor_cols))
+
+# normalize features
+pre_proc_val <- preProcess(train[,-1], method = c("center", "scale"))
+
+train[,-1] = predict(pre_proc_val, train[,-1])
+test[,-1] = predict(pre_proc_val, test[,-1])
+
+# model
+ctrl <- trainControl(method = "cv", number = 5, verboseIter = T)
+grid <- expand.grid(
+    alpha = 0, 
+    lambda = 10^seq(2, -3, by = -.1)
+)
+reg_team <- train(team_score ~., data = train,
+                  method = "glmnet",
+                  trControl = ctrl,
+                  tuneGrid = grid)
+
+reg_team
+summary(reg_team) # model components
+autoplot(reg_team$finalModel) # viz - ggfortify
+glance(reg_team$finalModel) # entire model - tidymodels
+tidy(reg_team$finalModel) # model components - tidymodels
+
+# predictions
+team_pred <- predict(reg_team, test)
+
+# model evaluation
+postResample(pred = team_pred, obs = test$team_score) # caret eval
+
+# variable importance
+importance <- varImp(reg_team, scale = F)
+plot(importance)
+
+reg_team_imp <- rownames_to_column(importance[["importance"]], "Var") %>%
+    arrange(desc(Overall)) %>%
+    head(20)
+reg_team_imp
+
+
+# opp score linear regression model ----
+
+# all features
+train <- nba_final %>%
+    filter(season <= 2021) %>%
+    select(opp_score, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+test <- nba_final %>%
+    filter(season > 2021) %>%
+    select(opp_score, away_implied_prob,
+           b2b_first:opp_b2b_second,
+           away_fg2m:away_fg3_pct,
+           away_fgm:away_opp_fg3_pct,
+           away_opp_fgm:away_opp_ftr,
+           away_opp_ast:away_pace,
+           home_fg2m:home_fg3_pct,
+           home_fgm:home_opp_fg3_pct,
+           home_opp_fgm:home_opp_ftr,
+           home_opp_ast:home_pace)
+
+# highly correlated features removed
+train <- train %>% select(-all_of(cor_cols))
+test <- test %>% select(-all_of(cor_cols))
+
+# normalize features
+pre_proc_val <- preProcess(train[,-1], method = c("center", "scale"))
+
+train[,-1] = predict(pre_proc_val, train[,-1])
+test[,-1] = predict(pre_proc_val, test[,-1])
+
+# model
+ctrl <- trainControl(method = "cv", number = 5, verboseIter = T)
+grid <- expand.grid(
+    alpha = 0, 
+    lambda = 10^seq(2, -3, by = -.1)
+)
+reg_opp <- train(opp_score ~., data = train,
+                 method = "glmnet",
+                 trControl = ctrl,
+                 tuneGrid = grid)
+
+reg_opp
+summary(reg_opp) # model components
+autoplot(reg_opp$finalModel) # viz - ggfortify
+glance(reg_opp$finalModel) # entire model - tidymodels
+tidy(reg_opp$finalModel) # model components - tidymodels
+
+# predictions
+opp_pred <- predict(reg_opp, test)
+
+# model evaluation
+postResample(pred = opp_pred, obs = test$opp_score) # caret eval
+
+# variable importance
+importance <- varImp(lin_opp, scale = F)
+plot(importance)
+
+reg_opp_imp <- rownames_to_column(importance[["importance"]], "Var") %>%
+    arrange(desc(Overall)) %>%
+    head(20)
+reg_opp_imp
+
+
 
 
 
